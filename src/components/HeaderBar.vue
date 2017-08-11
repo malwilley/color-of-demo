9342da11c10b3202253c297280a9006a5da9ce94
Malachi Willey <malwilley@gmail.com>
2017-08-11 17:13:10 -0400

    Add normalize.css and basscss utility styles

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="flex flex-between">
+        <h1>color-of-demo</h1>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'header-bar',
+  data() {
+    return {};
+  },
+};
+</script>
