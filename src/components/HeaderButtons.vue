592388b482a62c6d6e032f7d5be384b927844f8e
Malachi Willey <malwilley@gmail.com>
2017-08-22 08:27:43 -0400

    Store header buttons as dynamic data

--- src/components/HeaderButtons.vue
+++ src/components/HeaderButtons.vue
@@ -1,16 +1,33 @@
 <template>
-    <ul class="list-reset flex">
-          <li class="selected btn p1">api</li>
-          <li class="btn p1">palette</li>
-          <li class="btn p1"><a href="https://github.com/malwilley/color-of-demo" target="_blank">github</a></li>
-        </ul>
+  <ul class="list-reset flex">
+    <li v-for="button in buttons" v-bind:key="button.name" class="btn p1" v-bind:class="{selected: button.selected}">
+      <a v-bind:href="button.link" v-bind:target="{'_blank': button.external}">{{ button.name }}</a>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'header-buttons',
   data() {
-    return {};
+    return {
+      buttons: [
+        {
+          name: 'api',
+          selected: true,
+        },
+        {
+          name: 'palette',
+          selected: false,
+        },
+        {
+          name: 'github',
+          selected: false,
+          link: 'https://github.com/malwilley/color-of-demo',
+          external: true,
+        },
+      ],
+    };
   },
 };
 </script>
